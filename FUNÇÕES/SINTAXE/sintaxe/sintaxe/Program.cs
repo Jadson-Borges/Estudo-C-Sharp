17cb6a54dd1f05ca49154c80cae0f6e9f1089184
Jadson-Borges <jadsonborgesdecastro@hotmail.com>
2026-05-11 18:04:58 -0300

    Estrutura de repetição While

---
+++ FUNÇÕES/SINTAXE/sintaxe/sintaxe/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sintaxe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            int resultado = Maior(n1, n2, n3);
+            Console.WriteLine("O maior número é: " + resultado);
+        }
+
+        static int Maior(int a, int b, int c)
+        {
+            int m;
+
+            if (a > b && a > c)
+            {
+                m = a;
+            }
+            else if (b > c)
+            {
+                m = b;
+            }
+            else
+            {
+                m = c;
+            }
+            return m;
+        }
+    }
+}
---
+++ FUNÇÕES/While/While/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Faça um programa que leia um número e mostre a raiz quadrada desse número. Se o número for negativo, mostre uma mensagem dizendo que não é possível calcular a raiz quadrada de um número negativo.
+            Console.Write("Escreva um número:");
+
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            while (x >= 0) //enquanto o número for positivo ou zero, o programa irá calcular a raiz quadrada
+            {
+                double raiz = Math.Sqrt(x); //calcula a raiz quadrada do número
+                Console.WriteLine("sua raiz é: " + raiz.ToString("F3", CultureInfo.InvariantCulture));
+                Console.WriteLine("Digite outro numero");
+                x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            Console.WriteLine("Número negativo!");
+
+        }
+
+    }
+}
