638cb098fce2cc8993e864e496c1ab3d04c534c8
Jadson-Borges <jadsonborgesdecastro@hotmail.com>
2026-05-16 13:36:54 -0300

    Atualizando atividades praticas Media Trimestral

---
+++ ORIENTAÇÃO AO OBJETO/EXERCICIOS PRATICOS/Media Trimestral/Media Trimestral/Aluno.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media_Trimestral
+{
+    internal class Aluno
+    {
+       public string Nome;
+        public double n1, n2, n3;
+
+        public double Media()
+        {
+            return n1 + n2 + n3;
+        }
+
+        public void Situacao()
+        {
+            if (Media() >= 60.0) 
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado, Faltam " + (60.0 - Media()) + " pontos.");
+            }
+        }
+
+    }
+}
---
+++ ORIENTAÇÃO AO OBJETO/EXERCICIOS PRATICOS/Media Trimestral/Media Trimestral/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media_Trimestral
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.WriteLine("Digite o nome do aluno:");
+            aluno.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a primeira nota:");
+            aluno.n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite a segunda nota:");
+            aluno.n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite a terceira nota:");
+            aluno.n3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("NOTA FINAL: " + aluno.Media().ToString("F2", CultureInfo.InvariantCulture));
+            aluno.Situacao();
+
+        }
+    }
+}
