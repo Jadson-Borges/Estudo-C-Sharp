8f8645814ef6329b6ab8e22e9443599138477bc8
Jadson Borges <jadsonborgesdecastro@hotmail.com>
2026-05-18 18:42:42 -0300

    Atividade de metodo estatico em C#

---
+++ ORIENTAÇÃO AO OBJETO/Atividade Membros Estaticos/ATIVIDADE 01/ATIVIDADE 01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace ATIVIDADE_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual é a cotação do dólar? " + Dolar.ValorD);
+
+            Console.WriteLine("Quantos dólares você vai comprar? ");
+            double valorc = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("o Valor a ser pago em reais é = " + Dolar.ConversorDeMoeda(valorc).ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
---
+++ ORIENTAÇÃO AO OBJETO/Estrutura Estatica 2/Estrutura Condicional 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estrutura_Condicional_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculadora.Circuferencia(raio);
+            double volume = Calculadora.Volume(raio);
+
+            Console.WriteLine("Circuferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PI: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+        
+
+            
+        }
+    }
+}
