b23e1fd56fb4aef40f02af003877f4136d455b83
Jadson-Borges <jadsonborgesdecastro@hotmail.com>
2026-05-20 14:30:48 -0300

    Aula de Properties - Aula04, Topico 05

---
+++ ORIENTAÇÃO AO OBJETO/Construtores/Aula 04 Properties/Aula 04 Properties/Produtos.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula_04_Properties
+{
+    internal class Produtos
+    {
+        
+        private string _nome;
+        private double _valor;
+        private int _quantidade;
+
+        public Produtos() { }
+
+        public Produtos (string nome, double valor, int quantidade)
+        {
+            _nome = nome;
+            _valor = valor;
+            _quantidade = quantidade;
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && _nome.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        public double Valor {
+            get { return _valor; }
+        }
+
+        public int Quantidade
+        {
+            get { return _quantidade; }
+        }
+
+        public double ValorTotalEstoque {
+            get
+            {
+                return _valor * _quantidade;
+            }
+        }
+
+        public void AdicionarProd (int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+
+        public void RemoverProd (int quantidade)
+        {
+            _quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return _nome
+                + ", $ "
+                + _valor.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + _quantidade
+                +"unidades, Total: $ "
+                + ValorTotalEstoque.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
