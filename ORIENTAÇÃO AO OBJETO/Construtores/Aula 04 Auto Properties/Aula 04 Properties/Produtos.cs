ce2a8dcadff4a0294d15be2c43e2e1cdbbb4e391
Jadson-Borges <jadsonborgesdecastro@hotmail.com>
2026-05-20 18:17:50 -0300

    Atualizando repositorio Git Estudo dia 20-05-2026

---
+++ ORIENTAÇÃO AO OBJETO/Construtores/Aula 04 Auto Properties/Aula 04 Properties/Produtos.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula_04_AUto_Properties
+{
+    internal class Produtos
+    {
+        
+        private string _nome; // não vai ser feito Auto Properties pois possui logica particular
+        public double Valor { get; private set; } // Auto Properties
+        public int Quantidade { get; private set; }// Auto Properties
+
+        public Produtos() { }
+
+        public Produtos (string nome, double valor, int quantidade)
+        {
+            _nome = nome;
+            Valor = valor;
+            Quantidade = quantidade;
+        }
+
+        //logica particular da propriedade Nome
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        public double ValorTotalEstoque {
+            get
+            {
+                return Valor * Quantidade;
+            }
+        }
+
+        public void AdicionarProd (int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProd (int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return _nome
+                + ", $ "
+                + Valor.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                +"unidades, Total: $ "
+                + ValorTotalEstoque.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ORIENTAÇÃO AO OBJETO/Construtores/Aula 04 Auto Properties/Aula 04 Properties/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_04_AUto_Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produtos p = new Produtos();
+
+            p.Nome = "TV 4k";
+            
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Valor);
+            Console.WriteLine(p.Quantidade);
+            
+        }
+    }
+}
