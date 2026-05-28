578f343b2806f61bd42df4d3c11fc9d4b21e946b
Jadson-Borges <jadsonborgesdecastro@hotmail.com>
2026-05-28 08:48:51 -0300

    Atualizando repositorio com PROPS e Atividade de saldo bancario

---
+++ ORIENTAÇÃO AO OBJETO/Construtores/ATIVIDADES/ATIVIDADE SALDO BANCARIO/ATIVIDADE SALDO BANCARIO/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+
+namespace ATIVIDADE_SALDO_BANCARIO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre o numero da conta: ");
+            int numConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o Titular da conta: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            bool temSaldo = (Console.ReadLine().ToLower() == "s");
+
+            Conta conta;
+
+            if (temSaldo)
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                conta = new Conta(numConta, nome, saldo);
+            }
+            else
+            {
+                conta = new Conta(numConta, nome, 0);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Informe valor do depósito: ");
+            double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            conta.Deposito(deposito);
+
+            Console.WriteLine("Dados atualizados:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Informe o valor do saque: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            conta.Saque(saque);
+
+            Console.WriteLine("Dados atualizados:");
+            Console.WriteLine(conta);
+        }
+    }
+}
---
+++ ORIENTAÇÃO AO OBJETO/Construtores/ATIVIDADES/Correção de Atividade Saldo Bancario/Correção de Atividade Saldo Bancario/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.Remoting.Channels;
+
+
+namespace Correção_de_Atividade_Saldo_Bancario
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+            Conta conta;
+
+            Console.WriteLine("Informe o Numero da Conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o Titular da Conta: ");
+            string titular = Console.ReadLine();
+
+            Console.WriteLine("Tem Saldo inicial? (s/n)");
+            char resultado = char.Parse(Console.ReadLine());
+
+            //Caso o usuario digite s ou S no Terminal
+
+            if (resultado == 's' || resultado == 'S') 
+            {
+                Console.WriteLine("entre com o valor de Deposito inicial: ");
+                decimal saldo = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); 
+                conta = new Conta(numeroConta, titular, saldo);
+            }
+
+            //Caso não
+            else
+            {
+                conta = new Conta (numeroConta, titular);
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.WriteLine("Informe um valor de Deposito:");
+            decimal saldoConta = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(saldoConta);
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.WriteLine("Informe o valor a ser sacado:");
+            saldoConta = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(saldoConta);
+            Console.WriteLine(conta);
+
+        }
+
+    }
+}
