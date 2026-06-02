7819bc3e74a77d6ce00fd55f2fb6cdaba53a2ec2
Jadson Borges <jadsonborgesdecastro@hotmail.com>
2026-06-02 20:09:55 -0300

    Atualizando repositorio home aula de vetores dia 02-06-2026

---
+++ ORIENTAÇÃO AO OBJETO/Atividade Membros de Classe/Atividade Membros de Classe/Atividade Membros de Classe/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Globalization;
+
+namespace Atividade_Membros_de_Classe
+{
+    internal class Program
+    {
+        // =========================
+        // MÉTODO PRINCIPAL (ENTRY POINT)
+        // =========================
+        static void Main(string[] args)
+        {
+            // =========================
+            // ENTRADA DE DADOS
+            // =========================
+            Console.WriteLine("Entre com o numero d Conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o titulo da Conta: ");
+            string nomeConta = Console.ReadLine();
+
+            Console.WriteLine("Haverá deposito inicial (s/n)? ");
+            bool resposta = Console.ReadLine().Trim().ToLower() == "s";
+
+            // =========================
+            // DECLARAÇÃO DE OBJETO
+            // =========================
+            Conta conta;
+
+            // =========================
+            // ESTRUTURA CONDICIONAL
+            // =========================
+            if (resposta)
+            {
+                // =========================
+                // PROCESSAMENTO / REGRA DE NEGÓCIO
+                // =========================
+                Console.WriteLine("Informe o deposito inicial", CultureInfo.InvariantCulture);
+                decimal saldo = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                // =========================
+                // INSTANCIAÇÃO DE OBJETO
+                // =========================
+                conta = new Conta (numeroConta, nomeConta, saldo);
+
+                // =========================
+                // SAÍDA DE DADOS
+                // =========================
+                Console.WriteLine("Dados da Conta:");
+                Console.WriteLine(conta);
+
+                // =========================
+                // CHAMADA DE MÉTODO
+                // =========================
+                Console.WriteLine("Informe um novo deposito", CultureInfo.InvariantCulture);
+                saldo = decimal.Parse (Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta.Deposito(saldo);
+                Console.WriteLine(conta);
+
+                // =========================
+                // CHAMADA DE MÉTODO
+                // =========================
+                Console.WriteLine("Efetue um Saque", CultureInfo.InvariantCulture);
+                saldo = decimal.Parse (Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta.Saque(saldo);
+                Console.WriteLine(conta);
+            }
+
+            else 
+
+            {
+                conta = new Conta(numeroConta, nomeConta, 0);
+
+                Console.WriteLine("Dados da Conta:");
+                Console.WriteLine(conta);
+            }
+
+            Console.WriteLine("Fim do Programa!");
+        }
+    }   
+}
