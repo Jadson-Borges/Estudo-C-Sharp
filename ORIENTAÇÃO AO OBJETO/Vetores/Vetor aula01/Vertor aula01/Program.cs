f75fa11a7f176a5601d487c609fb4838868fb6f2
Jadson-Borges <jadsonborgesdecastro@hotmail.com>
2026-06-05 18:05:00 -0300

    Atualizando repositorio estou na aula 74 do curso de C#

---
+++ ORIENTAÇÃO AO OBJETO/Vetores/Vetor aula01/Vertor aula01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Net.Http.Headers;
+
+namespace Vertor_aula01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vect = new double [n];
+
+            for (int i=0; i<n; i++)
+            {
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            double soma = 0;
+            for (int i=0; i<n; i++)
+            {
+                soma += vect[i];
+            }
+
+            double media = soma / n;
+
+            Console.WriteLine("Resultado: " + media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
