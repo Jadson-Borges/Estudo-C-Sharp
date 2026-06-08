16e83c9e193871eba609f77af50410b30fb32ef1
Jadson-Borges <jadsonborgesdecastro@hotmail.com>
2026-06-08 18:03:10 -0300

    Estudo de Find dia 08-06-2026

---
+++ ORIENTAÇÃO AO OBJETO/Vetores/Foreach/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vetor = new string[] {"Maria", "Jadson", "Jose" };
+
+            //para cada - foreach substitui o FOR
+            foreach (string obj in vetor)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
