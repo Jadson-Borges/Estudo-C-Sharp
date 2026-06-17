9131e0d090667ec2a8fac531480fc7de23ecc6b7
Jadson-Borges <jadsonborgesdecastro@hotmail.com>
2026-06-17 11:14:31 -0300

    Atualizando repositorio focus 17-06-2026

---
+++ INFERENCIA DE TIPO/Expressao condicional ternaria/Expressao condicional ternaria/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressao_condicional_ternaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor = double.Parse(Console.ReadLine());
+            double desconto = (valor > 20) ? valor * 0.1 : valor * 0.05;
+            
+            Console.WriteLine(desconto);
+        }
+    }
+}
---
+++ TEMPO/DataTime/DataTime/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace DataTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime data = DateTime.Parse("2000-08-15");
+            DateTime data1 = DateTime.Parse("2000-08-15 13:05:58");
+            DateTime data2 = DateTime.Parse("15/08/2000 13:05:58");
+            
+
+            DateTime data3 = DateTime.ParseExact("2000-08-15","yyyy-MM-dd", CultureInfo.InvariantCulture);
+            DateTime data4 = DateTime.ParseExact("2000-08-15 13:05:58","yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine(data);
+            Console.WriteLine(data1);
+            Console.WriteLine(data2);
+            Console.WriteLine(data3);
+            Console.WriteLine(data4);
+
+        //    DateTime data1 = new DateTime();
+        //    DateTime data2 = new DateTime(2018, 11, 25);
+        //    DateTime data3 = new DateTime(2018, 11, 25, 20, 45, 3);
+        //    DateTime data4 = new DateTime(2018, 11, 25, 20, 45, 3, 500);
+
+        //    DateTime data5 = DateTime.Now;
+        //    DateTime data6 = DateTime.Today;
+        //    //horario universal
+        //    DateTime data7 = DateTime.UtcNow;
+
+
+
+        //    Console.WriteLine(data1);
+        //    Console.WriteLine(data2);
+        //    Console.WriteLine(data3);
+        //    Console.WriteLine(data4);
+        //    Console.WriteLine(data5);
+        //    Console.WriteLine(data6);
+        //    Console.WriteLine(data7);
+        }
+    }
+}
