cf10160d864b742c33533db81df2ecd7e1ec0028
Jadson-Borges <jadsonborgesdecastro@hotmail.com>
2026-07-16 16:18:04 -0300

    atividade de metodo abstrato 16/07/2026

---
+++ ORIENTAÇÃO AO OBJETO/EXERCICIOS PRATICOS/Metodo Abstrato/Metodo Abstrato/Entities/LegalEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metodo_Abstrato.Entities
+{
+    internal class LegalEntity : Taxpayer
+    {
+        public int NumberOfEmployees { get; set; }
+
+        public LegalEntity (string name, double annualIncome, int numberOfEmployees)
+            : base (name, annualIncome)
+        {
+            NumberOfEmployees = numberOfEmployees;
+        }
+
+        public override double Tax()
+        {
+            if (NumberOfEmployees <= 10)
+            {
+                return AnnualIncome * 0.16;
+            }
+            else
+            {
+                return AnnualIncome * 0.14;
+            }
+        }
+    }
+}
---
+++ ORIENTAÇÃO AO OBJETO/EXERCICIOS PRATICOS/Metodo Abstrato/Metodo Abstrato/Program.cs
@@ -0,0 +1,61 @@
+﻿using Metodo_Abstrato.Entities;
+using System.Globalization;
+namespace Metodo_Abstrato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Taxpayer> list = new List<Taxpayer>();
+
+            Console.WriteLine("Entre com o numero de Pagadores: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Contribuinte #{i} Dados: ");
+                Console.WriteLine("Pessoa Fisica ou empresa? (f/e): ");
+
+                char fe = char.Parse(Console.ReadLine());
+
+                Console.Write("Nome: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Renda Anual: ");
+                double annualIncome = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                
+
+                if (fe == 'e')
+                {
+                    Console.Write("Numero de Funcionarios: ");
+                    int numberOfEmployees = int.Parse(Console.ReadLine());
+
+                    list.Add(new LegalEntity(name, annualIncome, numberOfEmployees));
+                }
+                else
+                { 
+                Console.Write("Despesas de saúde: ");
+                double healthcareSpending = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                list.Add(new Individual(name, annualIncome, healthcareSpending));
+                }
+
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("TAXA PAGA:");
+
+
+            foreach (Taxpayer taxpayer in list)
+            {
+                
+                Console.Write(taxpayer.Name + ": " + taxpayer.Tax().ToString("f2", CultureInfo.InvariantCulture));
+            }
+
+
+        }
+    }
+}
