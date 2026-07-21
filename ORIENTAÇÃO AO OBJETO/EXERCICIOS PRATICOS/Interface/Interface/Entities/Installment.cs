c526c73132f020d8b40e4026b6113ed4f20fdb05
Jadson-Borges <jadsonborgesdecastro@hotmail.com>
2026-07-21 18:01:06 -0300

    atualizando repositorio dobre Interface

---
+++ ORIENTAÇÃO AO OBJETO/EXERCICIOS PRATICOS/Interface/Interface/Entities/Installment.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+namespace Interface.Entities
+{
+    internal class Installment
+    {
+        public DateTime DueDate { get; set; }
+        public double Ammount { get; set; }
+
+        public Installment (DateTime dueDate, double ammount)
+        {
+            DueDate = dueDate;
+            Ammount = ammount;
+        }
+
+        public override string ToString()
+        {
+            return DueDate.ToString("dd/MM/yyyy")
+                + " - "
+                + Ammount.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ORIENTAÇÃO AO OBJETO/EXERCICIOS PRATICOS/Interface/Interface/Services/IOnlinePaymentService.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Interface.Services
+{
+    internal interface IOnlinePaymentService
+    {
+        double Interest(double ammount, int months);
+        double PaymentFee(double ammount);
+    }
+}
